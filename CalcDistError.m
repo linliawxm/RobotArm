c0b0b2ece780573f1875de12d69d8fc068d694bc
FrankLin <linliawxm@gmail.com>
2021-10-07 19:36:21 -0400

    Only optimize some DH parameters, some parameters are 0 or -90 are treated as fixed. no need improvement.

--- CalcDistError.m
+++ CalcDistError.m
@@ -1,18 +1,20 @@
 function error = CalcDistError(dh)
+    %dh = [a1 a2 d1 d2 d3 offset1 offset2 baseY], other parameters are fixed,
+    %don't need to optimize
     clear L
     deg = pi/180;
 
     %Load measured distance data and 3 joint angles from file
     MeasData = load ('DistanceData.txt');
 
-    %1st column are parameter a
-    a=dh(:,1);
-    %2nd column are parameter alpha
-    alpha=dh(:,2);
-    %3rd column are parameter alpha
-    d=dh(:,3);
+    %build parameter a
+    a=[dh(1);dh(2);0];
+    %build parameter alpha
+    alpha=[0;-90;0];
+    %build parameter d
+    d=[dh(3);dh(4);dh(5)];
     %4th column are parameter alpha
-    offset=dh(:,4);
+    offset=[dh(6);dh(7);0];
     %define the links of robot
     L(1) = Revolute('d', d(1), 'a', a(1),'alpha', alpha(1)*deg, ... 
         'offset',offset(1)*deg ); 
@@ -23,7 +25,7 @@ function error = CalcDistError(dh)
     L(3) = Revolute('d', d(3), 'a', a(3), 'alpha', alpha(3)*deg,  ...
          'offset',offset(3)*deg  );
     %Create the robot
-    g8robot = SerialLink(L, 'name', 'G8 Robot Arm','base',[0,-0.08,0]);
+    g8robot = SerialLink(L, 'name', 'G8 Robot Arm','base',[0,dh(8),0]);
 
     theta = deg2rad(MeasData(:,1:3));
     MeasDist = MeasData(:,4)';
