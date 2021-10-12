35da0dd5cb05802257fcf48893282d29d58d0825
FrankLin <linliawxm@gmail.com>
2021-10-12 18:33:31 -0400

    optimize all DH parameters and offset, base X,Y,Z

--- CalcDistError.m
+++ CalcDistError.m
@@ -2,24 +2,28 @@
 %clipboard coordinate system origin
 %
 %Input parameter:
-% dh = [a1 a2 d1 d2 d3 offset1 offset2]
-% a1 a2 d1 d2 d3: DH parameters
-% offset1 offset2: kinematic joint coordinate offsets
-% baseY: clipboard coordinate system translation on Y axis
-% a3,alpha1,alpha2, alpha3, offset3 are treated as fixed value
-% a3 = 0; alpha1 = 0; alpha2 = -90; alpha3 = 0; offset3 = 0;
+% DH parameters of 3 links robot
+% dh = [a1 a2 a3;
+%       alpha1 alpha2 alpha3; 
+%       d1 d2 d3; 
+%       offset1 offset2 offset3;
+%       baseX baseY baseZ] 
 function error = CalcDistError(dh)
     clear L
     deg = pi/180;
-
+    
     %build parameter a
-    a=[dh(1);dh(2);0];
+    a=[dh(1,1);dh(1,2);dh(1,3)];
     %build parameter alpha
-    alpha=[0;-90;0];
+    alpha=[dh(2,1);dh(2,2);dh(2,3)];
     %build parameter d
-    d=[dh(3);dh(4);dh(5)];
+    d=[dh(3,1);dh(3,2);dh(3,3)];
     %4th column are parameter alpha
-    offset=[dh(6);dh(7);0];
+    offset=[dh(4,1);dh(4,2);dh(4,3)];
+    baseX = dh(5,1);
+    baseY = dh(5,2);
+    baseZ = dh(5,3);
+    
     %define the links of robot
     L(1) = Revolute('d', d(1), 'a', a(1),'alpha', alpha(1)*deg, ... 
         'offset',offset(1)*deg ); 
@@ -29,11 +33,14 @@ function error = CalcDistError(dh)
 
     L(3) = Revolute('d', d(3), 'a', a(3), 'alpha', alpha(3)*deg,  ...
          'offset',offset(3)*deg  );
+    
     %Create the robot
-    g8robot = SerialLink(L, 'name', 'G8 Robot Arm','base',transl(0,-0.08,0.047));
+    g8robot = SerialLink(L, 'name', 'G8 Robot Arm','base',transl(baseX,baseY,baseZ));
     figure(2)
     clf(2)
+    %Plot the robot
     g8robot.plot([0,0,0]);
+    
     %Load measured distance data and 3 joint angles from file
     MeasData = load ('DistanceData.txt');
     %get the joint angles of each pose
